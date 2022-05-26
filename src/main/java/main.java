cb78857ffcd5cba6829509c5bc2b7493d444f2cb
haithem <haithembenkhalef@gmail.com>
2022-05-26 11:00:29 +0100

    theme+connect button

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,23 +1,31 @@
+import com.formdev.flatlaf.IntelliJTheme;
+import com.formdev.flatlaf.intellijthemes.FlatArcOrangeIJTheme;
+import org.apache.commons.net.ftp.FTPClient;
+
 import java.io.*;
 import java.sql.*;
-import java.io.File;
-import java.io.FileInputStream;
-
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.Scanner;
 
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
+
+import javax.swing.*;
+
 public class main {
     public static void main(String[] args) {
 
 
-        test ts = new test();
-      //testwo ts2 = new testwo();
+        try {
+            UIManager.setLookAndFeel(new FlatArcOrangeIJTheme());
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
 
+
+
+
+        test ts = new test();
+        //testwo ts2 = new testwo();
 
 
         String server = "192.168.30.230";
@@ -25,23 +33,22 @@ public class main {
         String pass = "audaxis";
         FTPClient ftpClient = new FTPClient();
 
-        try{
+        try {
             File myObj = new File("table_file.txt");
             PrintWriter writer = new PrintWriter("table_file.txt", "UTF-8");
             Class.forName("oracle.jdbc.driver.OracleDriver");
 
 
-
-            Connection con=DriverManager.getConnection("jdbc:oracle:thin:@192.168.30.64:1521:TEST",
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.30.64:1521:TEST",
                     "dia",
                     "admin");
 
 
-            Statement stmt=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
                     ResultSet.CONCUR_READ_ONLY);
 
 
-            ResultSet rs=stmt.executeQuery("select * from ad_user");
+            ResultSet rs = stmt.executeQuery("select * from ad_user");
 
             ResultSetMetaData rsmd = rs.getMetaData();
 
@@ -49,13 +56,13 @@ public class main {
             int counter = 1;
             while (rs.next()) {
 
-                for(int i = 1 ; i <= columnsNumber; i++){
+                for (int i = 1; i <= columnsNumber; i++) {
 
-                   // System.out.print(rs.getString(i) + " ;"); //Print one element of a row
+                    // System.out.print(rs.getString(i) + " ;"); //Print one element of a row
                     writer.print(rs.getString(i) + " ;");
                 }
 
-              //  System.out.println();//Move to the next line to print the next row.
+                //  System.out.println();//Move to the next line to print the next row.
                 writer.println();
             }
 
@@ -73,8 +80,8 @@ public class main {
 
             try {
 
-                File fis=new File("table_file.txt");
-                Scanner sc=new Scanner(fis);
+                File fis = new File("table_file.txt");
+                Scanner sc = new Scanner(fis);
 
 
                 String strLine;
@@ -90,14 +97,13 @@ public class main {
                 sc.close();
 
 
-
                 ResultSet rss = stmt.executeQuery("select ad_user_id from ad_user"); // Get your ResultSet from Database
 
                 rss.last();                  // Place the record pointer onto the last row
                 int count = rss.getRow();    // System.out.println("this is count"+count); // Get the row number (there's your count)
                 rss.first();                 // Place the record pointer onto the first row for the while loop
-                String[] myArray = new String[count+1]; // Declare and Initialize your array
-                myArray[0]="0";
+                String[] myArray = new String[count + 1]; // Declare and Initialize your array
+                myArray[0] = "0";
                 counter = 1; // Reset counter to 0 so as to act as a Index incrementer
                 // Iterate through the ResultSet and fill Array
                 while (rss.next()) {
@@ -116,77 +122,82 @@ public class main {
                 }*/
 
                 for (int i = 0; i < list.size(); i++) {
-                   // System.out.println("this is the list size"+list.size());
+                    // System.out.println("this is the list size"+list.size());
                     strLine = list.get(i).toString();
                     String[] values = strLine.split(" ;");
                     /*for (int b=0 ;b< values.length;b++){
                         System.out.println("newvalue"+values[b]);
                     }*/
 
-                    String  AD_USER_ID=values[0],
-                            AD_CLIENT_ID=values[1],
-                            AD_ORG_ID=values[2],
-                            ISACTIVE=values[3],
-                            CREATED=values[4],
-                            CREATEDBY=values[5],
-                            UPDATED=values[6],
-                            UPDATEDBY=values[7],
-                            NAME=values[8],
-                            DESCRIPTION=values[9],
-                            PASSWORD=values[10],
-                            EMAIL=values[11],
-                            SUPERVISOR_ID=values[12],
-                            C_BPARTNER_ID=values[13],
-                            PROCESSING=values[14],
-                            EMAILUSER=values[15],
-                            EMAILUSERPW=values[16],
-                            C_BPARTNER_LOCATION_ID=values[17],
-                            C_GREETING_ID=values[18],
-                            TITLE=values[19],
-                            COMMENTS=values[20],
-                            PHONE=values[21],
-                            PHONE2=values[22],
-                            FAX=values[23],
-                            LASTCONTACT=values[24],
-                            LASTRESULT=values[25],
-                            BIRTHDAY=values[26],
-                            AD_ORGTRX_ID=values[27],
-                            EMAILVERIFY=values[28],
-                            C_JOB_ID=values[29],
-                            EMAILVERIFYDATE=values[30],
-                            ISFULLBPACCESS=values[31],
-                            LDAPUSER=values[32],
-                            NOTIFICATIONTYPE=values[33],
-                            CONNECTIONPROFILE=values[34],
-                            VALUE=values[35],
-                            AD_TREE_MENUFAVORITE_ID=values[36],
-                            AD_TREE_MENUNEW_ID=values[37],
-                            BOUNCEDINFO=values[38],
-                            ISEMAILBOUNCED=values[39];
-                            String LASTREGISTRATIONREMINDER;
-                            if (values[40]=="null"){  LASTREGISTRATIONREMINDER=null;}else{ LASTREGISTRATIONREMINDER=values[40];}
+                    String AD_USER_ID = values[0],
+                            AD_CLIENT_ID = values[1],
+                            AD_ORG_ID = values[2],
+                            ISACTIVE = values[3],
+                            CREATED = values[4],
+                            CREATEDBY = values[5],
+                            UPDATED = values[6],
+                            UPDATEDBY = values[7],
+                            NAME = values[8],
+                            DESCRIPTION = values[9],
+                            PASSWORD = values[10],
+                            EMAIL = values[11],
+                            SUPERVISOR_ID = values[12],
+                            C_BPARTNER_ID = values[13],
+                            PROCESSING = values[14],
+                            EMAILUSER = values[15],
+                            EMAILUSERPW = values[16],
+                            C_BPARTNER_LOCATION_ID = values[17],
+                            C_GREETING_ID = values[18],
+                            TITLE = values[19],
+                            COMMENTS = values[20],
+                            PHONE = values[21],
+                            PHONE2 = values[22],
+                            FAX = values[23],
+                            LASTCONTACT = values[24],
+                            LASTRESULT = values[25],
+                            BIRTHDAY = values[26],
+                            AD_ORGTRX_ID = values[27],
+                            EMAILVERIFY = values[28],
+                            C_JOB_ID = values[29],
+                            EMAILVERIFYDATE = values[30],
+                            ISFULLBPACCESS = values[31],
+                            LDAPUSER = values[32],
+                            NOTIFICATIONTYPE = values[33],
+                            CONNECTIONPROFILE = values[34],
+                            VALUE = values[35],
+                            AD_TREE_MENUFAVORITE_ID = values[36],
+                            AD_TREE_MENUNEW_ID = values[37],
+                            BOUNCEDINFO = values[38],
+                            ISEMAILBOUNCED = values[39];
+                    String LASTREGISTRATIONREMINDER;
+                    if (values[40] == "null") {
+                        LASTREGISTRATIONREMINDER = null;
+                    } else {
+                        LASTREGISTRATIONREMINDER = values[40];
+                    }
 
 
-                   // System.out.println("THIS IS THE DATE WHEN IS WAS CREATED"+AD_USER_ID+"somethimg");
+                    // System.out.println("THIS IS THE DATE WHEN IS WAS CREATED"+AD_USER_ID+"somethimg");
 
-                    if(Arrays.asList(myArray).contains(AD_USER_ID) /*|| AD_USER_ID=="0"*/) {
+                    if (Arrays.asList(myArray).contains(AD_USER_ID) /*|| AD_USER_ID=="0"*/) {
                         System.out.println("this user already exists");
-                    }else {
-                        stmt.executeUpdate("insert into AD_USER values ('" + AD_USER_ID + "','1000000' ,'1000000', '"+ISACTIVE+"' ,TO_DATE(' " + CREATED + "   ', 'yyyy-mm-dd hh24:mi:ss') ,'100' ,TO_DATE('2010-03-09 12:39:06' ,'yyyy-mm-dd hh:mi:ss'), '100', 'Pierre' ,null, '5e0c7831afecea6a' , '" + EMAIL + "', null ,'1000683' ,'N' ,'psp@audaxis.com','d7f2ac10a0321ab42c48267ccd969c98' ,'1000553',null, null ,null, null ,null ,null, null, null ,null, null, null ,null ,null ,'Y' ,null, 'E',null ,'pierre' ,null ,null ,null ,'N' ," + LASTREGISTRATIONREMINDER + " )");
-                        System.out.println("added the user with ;"+AD_USER_ID);
+                    } else {
+                        stmt.executeUpdate("insert into AD_USER values ('" + AD_USER_ID + "','1000000' ,'1000000', '" + ISACTIVE + "' ,TO_DATE(' " + CREATED + "   ', 'yyyy-mm-dd hh24:mi:ss') ,'100' ,TO_DATE('2010-03-09 12:39:06' ,'yyyy-mm-dd hh:mi:ss'), '100', 'Pierre' ,null, '5e0c7831afecea6a' , '" + EMAIL + "', null ,'1000683' ,'N' ,'psp@audaxis.com','d7f2ac10a0321ab42c48267ccd969c98' ,'1000553',null, null ,null, null ,null ,null, null, null ,null, null, null ,null ,null ,'Y' ,null, 'E',null ,'pierre' ,null ,null ,null ,'N' ," + LASTREGISTRATIONREMINDER + " )");
+                        System.out.println("added the user with ;" + AD_USER_ID);
                     }
 
 
                 }
 
 
-            }catch(IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
             con.close();
 
-        }catch(Exception e){ System.out.println(e);}
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,18 +1,10 @@
-import javafx.scene.paint.Color;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.*;
-import java.io.*;
-import java.sql.*;
-import java.io.File;
 
 
-public class test {
+public class test extends JFrame {
 
     private JPanel panel1;
     private JButton connectButton;
@@ -24,60 +16,28 @@ public class test {
     private JPasswordField passwordField1;
 
     public test() {
-        JFrame frame = new JFrame("test");
-        frame.setSize(8900,5000);
-        frame.setContentPane(this.panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+
+        setSize(8900,5000);
+        setContentPane(this.panel1);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
         connectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
                 try {
-                    File myObj = new File("table_file.txt");
-                    PrintWriter writer = new PrintWriter("table_file.txt", "UTF-8");
                     Class.forName("oracle.jdbc.driver.OracleDriver");
-
-
-
-                    Connection con= DriverManager.getConnection("jdbc:oracle:thin:@192.168.30.14:1521:TEST",
-                            "dia",
-                            "admin");
-
-
-                    Statement stmt=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-                            ResultSet.CONCUR_READ_ONLY);
-
-
-                    ResultSet rs=stmt.executeQuery("select * from ad_user");
-
-                    ResultSetMetaData rsmd = rs.getMetaData();
-
-                    int columnsNumber = rsmd.getColumnCount();
-                    int counter = 1;
-                    while (rs.next()) {
-
-                        for(int i = 1 ; i <= columnsNumber; i++){
-
-                            System.out.print(rs.getString(i) + " ;"); //Print one element of a row
-                            writer.print(rs.getString(i) + " ;");
-                        }
-
-                        System.out.println();//Move to the next line to print the next row.
-                        writer.println();
-                    }
-
-                }catch(IOException | ClassNotFoundException exx)
-                {
+                    Connection con = DriverManager.getConnection("jdbc:oracle:thin:@" + textField3.getText() + ":" + textField2.getText() + ":" + textField4.getText(),
+                            textField1.getText(),
+                            passwordField1.getText());
+                } catch (ClassNotFoundException exx) {
                     exx.printStackTrace();
                 } catch (SQLException ex) {
                     throw new RuntimeException(ex);
                 }
-
-
                 testwo ts2 = new testwo();
-                frame.setVisible(false);
+                test.this.dispose();
+
             }
         });
         testConnactionButton.addActionListener(new ActionListener() {
@@ -90,10 +50,10 @@ public class test {
                     Statement stmt = con.createStatement();
                     ResultSet rs = stmt.executeQuery("select * from ad_user");
                     if(rs.next()){
-                        JOptionPane.showMessageDialog(frame,"Connection Established");
+                        JOptionPane.showMessageDialog(test.this,"Connection Established");
                     }
                 } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(frame,ex.getMessage());
+                    JOptionPane.showMessageDialog(test.this,ex.getMessage());
                     //lblMessage.setText(e.getMessage());
                     //lblMessage.setTextFill(Color.RED);
                     System.out.println(ex);
